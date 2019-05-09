056431963b6b7a32d7022e674c60cc0ad6787714
Mark Murphy <markdmurphy@ymail.com>
2019-05-09 12:41:28 -0700

    Refactoring Dashboard and added Timeline

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,9 @@
 class DashboardsController < ApplicationController
 
     def show
-        @shout = Shout.new
-        @shouts = current_user.timeline_shouts
+        #@shout = Shout.new
+        #@shouts = current_user.timeline_shouts
+        @dashboard = Dashboard.new(current_user)
     end
 
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -5,8 +5,6 @@ class Shout < ApplicationRecord
   
   validates :user, presence: true
 
-  default_scope { order(created_at: :desc)}
-
   delegate :username, to: :user
 
 end
