a741f08bd4ba4b93c982d3a0164db627a50f01e4
Mark Murphy <markdmurphy@ymail.com>
2019-05-08 17:28:11 -0700

    Added Counters

---
+++ db/migrate/20190509002028_add_following_relationship_counter_caches_to_users.rb
@@ -0,0 +1,7 @@
+class AddFollowingRelationshipCounterCachesToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :followed_users_count, :integer, default: 0, null: false
+    add_column :users, :followers_count, :integer, default: 0, null: false
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_215613) do
+ActiveRecord::Schema.define(version: 2019_05_09_002028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,8 @@ ActiveRecord::Schema.define(version: 2019_05_08_215613) do
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
     t.string "username"
+    t.integer "followed_users_count", default: 0, null: false
+    t.integer "followers_count", default: 0, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
     t.index ["username"], name: "index_users_on_username", unique: true
